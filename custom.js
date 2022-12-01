d98807b552ef2285741c926687ab4cc27e64879b
manikanta-143 <manikanta7013@gmail.com>
2022-12-01 22:35:22 +0530

    done

---
+++ custom.js
@@ -0,0 +1,63 @@
+
+
+// let element=$('h1')
+// console.log(element)
+
+// $ ('h1').text("changed")
+
+// $ ('div').html("<p>modified</p>")
+// $ ('div').text("<p>modified</p>")
+
+// let s=$(".special");
+// $(s[0]).text("changed");
+
+// $('input[type="text"]').val("mani")
+// $('input[type="button"]').val("mani")
+
+// let s=$('.special').text()
+// alert(s)
+
+
+//   ANIMATION ///////
+// $('h1').hide(1000);
+// $('h1').hide();
+   
+// $('#show').on("click",()=>{
+//     $('h1').show(1000,()=>{
+//         alert()
+//     })
+// })
+
+// $('#hide').on("click",()=>{
+//     $('h1').hide(1000,()=>{
+//         alert("hello")
+//     })
+// })
+
+// $('#fadein').on("click",()=>{
+//     $('h1').fadeIn(1000)
+// })
+
+// $('#hide').on("click",()=>{
+//     $('h1').hide(1000)
+// })
+// $('#fadeout').on("click",()=>{
+//     $('h1').fadeOut(1000)
+// })
+
+// $('#slideup').on("click",()=>{
+//     $('h1').slideUp(1000)
+// })
+
+// $('#slidedown').on("click",()=>{
+//     $('h1').slideToggle(1000)
+// })
+// $('#animate').on("click",()=>{
+//     $('h1').animate({'background-color':'green','color':'blue'})
+// })
+
+
+
+//      TRAVERSAL   /////////
+
+
